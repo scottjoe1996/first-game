a3de6415ecabe5cc262e8010e24120837f061c97
Joseph <scottjoe1996@yahoo.co.uk>
2020-04-11 19:15:16 +0100

    added playermovement test

---
+++ First Game Project/Assets/Tests/EditMode/PlayerMovementTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class PlayerMovementTests
+    {
+        [Test]
+        public void PlayerMovementTestsSimplePasses()
+        {
+            var player = new GameObject().AddComponent<PlayerControl>();
+            PlayerMovement playerMovement = new PlayerMovement(player.transform, 12f);
+
+            Vector3 expected = new Vector3(12f, 0f, 12f);
+
+            Assert.AreEqual(expected, playerMovement.Calculate(1f, 1f, 1f));
+        }
+    }
+}
